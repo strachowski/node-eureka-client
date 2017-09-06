fb9728835a21d667c6634e9762d4f0e1690a6ba5
Roman Strachowski <stra@MacBook-Pro.local>
2017-09-06 12:17:09 +0530

    added random port to be able to spawn multiple instances

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const Eureka = require('eureka-js-client').Eureka;
- 
+
+var port = Math.floor(Math.random() * 99) + 8800  ;
+
 // example configuration 
 const client = new Eureka({
   // application instance information 
   instance: {
-    app: 'node-eureka-client',
+    app: 'node-eureka-client-'+port,
     hostName: 'localhost',
     ipAddr: '127.0.0.1',
     port: {
-        '$': 8080,
+        '$': port,
         '@enabled': true,
       },
     vipAddress: 'jq.test.something.com',
